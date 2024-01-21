b24d48f81a458984cd2737f72bb1d9f201516cea
Dewa Bisma <idewagedebisma@gmail.com>
2024-01-21 20:08:08 +0800

    feat: finish prettier

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,14 @@
-import "../src/app/globals.css";
+import '../src/app/globals.css';
 
-import type { Preview } from "@storybook/react";
-
-import { initialize, mswLoader } from "msw-storybook-addon";
+import type { Preview } from '@storybook/react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { initialize, mswLoader } from 'msw-storybook-addon';
 
 initialize();
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
