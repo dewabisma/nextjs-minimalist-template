54d5042d65f6b6812cf9f7d72a2dc601bd9fa7af
Dewa Bisma <idewagedebisma@gmail.com>
2024-01-21 20:44:48 +0800

    fix: fix setup error

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://leb-register.netlify.app', // FIXME: Change to the production URL
-  generateRobotsTxt: true,
-};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [te, setTe] = useState(false);
-
-  useEffect(() => {
-    if (te) setTe(true);
-  }, []);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
